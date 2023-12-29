556b9e32e17fa8075325305f1dac2cfcce9212f5
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-29 23:33:47 +0300

    добавлена модель Habit

---
+++ habits/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Habit(models.Model):
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    place = models.CharField(max_length=155, verbose_name="место")
+    action = models.CharField(max_length=155, verbose_name="действие")
+    schedule = models.DateField(verbose_name="время")
+    is_pleasant = models.BooleanField(verbose_name="приятная")
+    related_habit = models.ForeignKey('self', on_delete=models.CASCADE, verbose_name="связана")
+    frequency = models.PositiveSmallIntegerField(verbose_name="периодичность (дни)", default=1)
+    reward = models.CharField(max_length=255, verbose_name="награда")
+    duration = models.DurationField(verbose_name="продолжительность")
+    is_public = models.BooleanField(verbose_name="публичная", default=False)
+
+    class Meta:
+        verbose_name = 'привычка'
+        verbose_name_plural = 'привычки'
+
+    def __str__(self):
+        return f"{self.action}"
+
