eef5f4baea5ec28175d34a8a8ec9b2b50137ce43
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-30 00:01:55 +0300

    fix HabitViewSet

--- habits/views.py
+++ habits/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ModelViewSet
 
 from habits.models import Habit
 from habits.serializers import HabitSerializer
 
 
-class HabitViewSet(ViewSet):
-    model = Habit
+class HabitViewSet(ModelViewSet):
     serializer_class = HabitSerializer
     queryset = Habit.objects.all()
 
