6b207c9f5da2163ed242eec977d530a68803ba0d
Maxim Donchuk <m.donchuk@gmail.com>
2024-01-03 21:11:51 +0300

    добавлен валидатор для модели Habit, проверяющий что длительность привычки должна быть в диапазоне от 0 до 120 сек

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,12 +1,11 @@
 from rest_framework import serializers
 
 from habits.models import Habit
-from habits.validators import pleasant_xor_related
+from habits.validators import pleasant_xor_related, duration_less_than_120s
 
 
 class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
-        fields = '__all__'
-        validators = [pleasant_xor_related]
-
+        fields = "__all__"
+        validators = [pleasant_xor_related, duration_less_than_120s]
--- habits/validators.py
+++ habits/validators.py
@@ -1,6 +1,18 @@
+from datetime import timedelta
+
 from rest_framework import serializers
 
 
 def pleasant_xor_related(habit):
-    if not bool(habit['reward']) ^ (habit['related_habit'] is not None):
-        raise serializers.ValidationError('Choose a related habit or a reward, but not both.')
+    """Habit can have reward, or related habit, but only something one"""
+    if bool(habit["reward"]) & (habit["related_habit"] is not None):
+        raise serializers.ValidationError(
+            "Choose a related habit or a reward, but not both."
+        )
+
+
+def duration_less_than_120s(habit):
+    """Habit duration can be only in (0, 120] seconds"""
+    if not timedelta(seconds=0) < habit["duration"] <= timedelta(seconds=120):
+        raise serializers.ValidationError("Duration must be positive and less than 2 min")
+
