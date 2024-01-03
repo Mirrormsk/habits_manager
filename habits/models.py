3966476a0f80acb927e8570902f36e442ef929c7
Maxim Donchuk <m.donchuk@gmail.com>
2024-01-03 19:40:37 +0300

    связанная привычка может отсутствовать

--- habits/models.py
+++ habits/models.py
@@ -7,17 +7,20 @@ class Habit(models.Model):
     action = models.CharField(max_length=155, verbose_name="действие")
     schedule = models.TimeField(verbose_name="время")
     is_pleasant = models.BooleanField(verbose_name="приятная")
-    related_habit = models.ForeignKey('self', on_delete=models.CASCADE, verbose_name="связана")
-    frequency = models.PositiveSmallIntegerField(verbose_name="периодичность (дни)", default=1)
+    related_habit = models.ForeignKey(
+        "self", on_delete=models.CASCADE, verbose_name="связана", null=True, blank=True
+    )
+    frequency = models.PositiveSmallIntegerField(
+        verbose_name="периодичность (дни)", default=1
+    )
     reward = models.CharField(max_length=255, verbose_name="награда")
     duration = models.DurationField(verbose_name="продолжительность")
     is_public = models.BooleanField(verbose_name="публичная", default=False)
     last_sent = models.DateTimeField(verbose_name="отправлено", null=True, blank=True)
 
     class Meta:
-        verbose_name = 'привычка'
-        verbose_name_plural = 'привычки'
+        verbose_name = "привычка"
+        verbose_name_plural = "привычки"
 
     def __str__(self):
         return f"{self.action}"
-
