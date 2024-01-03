356777710053ace8f817b8db57dfc606c2f7edbe
Maxim Donchuk <m.donchuk@gmail.com>
2024-01-03 19:40:14 +0300

    добавлен валидатор исключающий одновременный выбор связанной привычки и указания вознаграждения.

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from habits.models import Habit
+from habits.validators import pleasant_xor_related
 
 
 class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = '__all__'
+        validators = [pleasant_xor_related]
+
