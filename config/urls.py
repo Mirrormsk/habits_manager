7bdd7b5369446b4019fe6cccc6ff1312f573164f
Maxim Donchuk <m.donchuk@gmail.com>
2024-01-04 13:45:43 +0300

    добавлены users.urls в корневые урлы

--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
         title="Habits API Documentation",
-        default_version='v1',
+        default_version="v1",
         description="Api documentation for Habits API",
         terms_of_service="https://www.example.com/policies/terms/",
         contact=openapi.Contact(email="m.donchuk@gmail.com"),
@@ -20,6 +20,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('habits.urls', namespace='habits')),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path("", include("habits.urls", namespace="habits")),
+    path("", include("users.urls", namespace="users")),
+    path("docs/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
 ]
