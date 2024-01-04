51349a5d60b2642cc9fba046c2061c0abb03ef25
Maxim Donchuk <m.donchuk@gmail.com>
2024-01-04 14:17:15 +0300

    исправлена ошибка в классе IsOwner

--- habits/permissions.py
+++ habits/permissions.py
@@ -4,4 +4,4 @@ from rest_framework import permissions
 class IsOwner(permissions.BasePermission):
     """Checks if the user is an owner of the habit or superuser"""
     def has_object_permission(self, request, view, obj):
-        return request.user and (request.user == obj.owner or request.user.is_superuser)
+        return request.user and (request.user == obj.user or request.user.is_superuser)
