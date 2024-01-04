3eb5a0f7a3eaa952f936b4c876c1dcf212810483
Maxim Donchuk <m.donchuk@gmail.com>
2024-01-04 13:47:01 +0300

    добавлен ViewSet для User

--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+from users.models import User
+from users.permissions import IsProfileOwnerOrReadOnly
+from users.serializers import UserSerializer, UserPublicSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated, IsProfileOwnerOrReadOnly]
+
+    def get_serializer_class(self):
+        user = self.request.user
+
+        if self.action == "retrieve":
+            instance = self.get_object()
+            if instance == user:
+                return UserSerializer
+            return UserPublicSerializer
+
+        elif self.action == "list":
+            return UserPublicSerializer
+
+        elif self.action in ["create", "update", "partial_update", "destroy"]:
+            return UserSerializer
+
+    def get_permissions(self):
+        if self.action == "create":
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
