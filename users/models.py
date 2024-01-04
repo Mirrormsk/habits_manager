5247e6dd90ce12754eba891b22b3b583bfd04271
Maxim Donchuk <m.donchuk@gmail.com>
2024-01-04 20:22:33 +0300

    для модели пользователя добавлены chat_id и tg_invite_link

--- users/models.py
+++ users/models.py
@@ -12,6 +12,8 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, verbose_name='почта')
     city = models.CharField(max_length=100, verbose_name='город', **NULLABLE)
     uid = models.UUIDField(default=uuid.uuid4, editable=False)
+    chat_id = models.PositiveIntegerField(verbose_name='telegram chat id', **NULLABLE)
+    tg_invite_link = models.URLField(**NULLABLE, verbose_name='telegram invite')
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
