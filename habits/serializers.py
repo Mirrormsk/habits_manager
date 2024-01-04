d14cda3cddd0b0e0aa489bcbf9d61c2532d8f95d
Maxim Donchuk <m.donchuk@gmail.com>
2024-01-04 11:27:40 +0300

    добавлен валидатор, проверяющий что только приятная привычка может быть связанной

--- habits/serializers.py
+++ habits/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
 
 from habits.models import Habit
-from habits.validators import pleasant_xor_related, duration_less_than_120s
+from habits.validators import (
+    pleasant_xor_related,
+    duration_less_than_120s,
+    only_pleasant_in_related,
+)
 
 
 class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = "__all__"
-        validators = [pleasant_xor_related, duration_less_than_120s]
+        validators = [
+            pleasant_xor_related,
+            duration_less_than_120s,
+            only_pleasant_in_related,
+        ]
--- habits/validators.py
+++ habits/validators.py
@@ -5,7 +5,8 @@ from rest_framework import serializers
 
 def pleasant_xor_related(habit):
     """Habit can have reward, or related habit, but only something one"""
-    if bool(habit["reward"]) & (habit["related_habit"] is not None):
+    habit = dict(habit)
+    if habit.get("reward") and habit.get("related_habit"):
         raise serializers.ValidationError(
             "Choose a related habit or a reward, but not both."
         )
@@ -16,3 +17,10 @@ def duration_less_than_120s(habit):
     if not timedelta(seconds=0) < habit["duration"] <= timedelta(seconds=120):
         raise serializers.ValidationError("Duration must be positive and less than 2 min")
 
+
+def only_pleasant_in_related(habit):
+    """Only pleasant habit can be chosen for a related habit"""
+    habit = dict(habit)
+    if related := habit.get("related_habit"):
+        if not related.is_pleasant:
+            raise serializers.ValidationError("Only pleasant habit can be chosen for a related habit")
