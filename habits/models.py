d6ee66f41e59e74aa2c382c31b92efc7e52bcf25
Maxim Donchuk <m.donchuk@gmail.com>
2024-01-04 21:55:10 +0300

    расписание - необязательное поле

--- habits/models.py
+++ habits/models.py
@@ -5,7 +5,7 @@ class Habit(models.Model):
     user = models.ForeignKey("users.User", on_delete=models.CASCADE)
     place = models.CharField(max_length=155, verbose_name="место")
     action = models.CharField(max_length=155, verbose_name="действие")
-    schedule = models.TimeField(verbose_name="время")
+    schedule = models.TimeField(verbose_name="время", null=True, blank=True)
     is_pleasant = models.BooleanField(verbose_name="приятная")
     related_habit = models.ForeignKey(
         "self", on_delete=models.CASCADE, verbose_name="связана", null=True, blank=True
